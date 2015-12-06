c532b27c986607dea37346800092236d5525b8d2
T. A <baranov@digitalmigital.ru>
2015-12-06 22:42:38 +0300

    Update admin.py
    1

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.sites.models import RequestSite
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
