6c4e747511cec7a326278d70e775f64011275b80
T. A <baranov@digitalmigital.ru>
2015-12-06 23:03:56 +0300

    Update auth_urls.py
    3

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -30,49 +30,48 @@ from django.core.urlresolvers import reverse_lazy
 
 from django.contrib.auth import views as auth_views
 
-
-urlpatterns = patterns('',
-                       url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html'},
-                           name='auth_login'),
-                       url(r'^logout/$',
-                           auth_views.logout,
-                           {'template_name': 'registration/logout.html'},
-                           name='auth_logout'),
-                       url(r'^password/change/$',
-                           auth_views.password_change,
-                           {'post_change_redirect': reverse_lazy('auth_password_change_done')},
-                           name='auth_password_change'),
-                       url(r'^password/change/done/$',
-                           auth_views.password_change_done,
-                           name='auth_password_change_done'),
-                       url(r'^password/reset/$',
-                           auth_views.password_reset,
-                           {'post_reset_redirect': reverse_lazy('auth_password_reset_done')},
-                           name='auth_password_reset'),
-                       url(r'^password/reset/complete/$',
-                           auth_views.password_reset_complete,
-                           name='auth_password_reset_complete'),
-                       url(r'^password/reset/done/$',
-                           auth_views.password_reset_done,
-                           name='auth_password_reset_done'),
-)
-
+urlpatterns = [
+    url(r'^login/$',
+        auth_views.login,
+        {'template_name': 'registration/login.html'},
+        name='auth_login'),
+    url(r'^logout/$',
+        auth_views.logout,
+        {'template_name': 'registration/logout.html'},
+        name='auth_logout'),
+    url(r'^password/change/$',
+        auth_views.password_change,
+        {'post_change_redirect': reverse_lazy('auth_password_change_done')},
+        name='auth_password_change'),
+    url(r'^password/change/done/$',
+        auth_views.password_change_done,
+        name='auth_password_change_done'),
+    url(r'^password/reset/$',
+        auth_views.password_reset,
+        {'post_reset_redirect': reverse_lazy('auth_password_reset_done')},
+        name='auth_password_reset'),
+    url(r'^password/reset/complete/$',
+        auth_views.password_reset_complete,
+        name='auth_password_reset_complete'),
+    url(r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        name='auth_password_reset_done'),
+]
 
 from django import get_version
 from distutils.version import LooseVersion
+
 if (LooseVersion(get_version()) >= LooseVersion('1.6')):
-    urlpatterns += patterns('',
-                            url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-                                auth_views.password_reset_confirm,
-                            {'post_reset_redirect': reverse_lazy('auth_password_reset_complete')},
-                                name='auth_password_reset_confirm')
-                        )
+    urlpatterns += [
+        url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+            auth_views.password_reset_confirm,
+            {'post_reset_redirect': reverse_lazy('auth_password_reset_complete')},
+            name='auth_password_reset_confirm')
+    ]
 else:
-    urlpatterns += patterns('',
-                            url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-                                auth_views.password_reset_confirm,
-                            {'post_reset_redirect': reverse_lazy('auth_password_reset_complete')},
-                                name='auth_password_reset_confirm')
-                        )
+    urlpatterns += [
+        url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            auth_views.password_reset_confirm,
+            {'post_reset_redirect': reverse_lazy('auth_password_reset_complete')},
+            name='auth_password_reset_confirm')
+    ]
